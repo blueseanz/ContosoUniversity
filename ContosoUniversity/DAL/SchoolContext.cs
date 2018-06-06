ebe97ccf6e0341a9c71148ca8e267dd12d754392
blueseanz <Walter.FengNZ@gmail.com>
2018-06-06 18:07:45 +1200

    Revert "test"
    This reverts commit 5ba7a79cdc6992261a6176a923d5510c714bf64f.

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.DAL
     public class SchoolContext : DbContext
     {
 
-        public SchoolContext() : base("SchoolContexteeeee")
+        public SchoolContext() : base("SchoolContext")
         {
         }
 
