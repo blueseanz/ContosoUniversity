75ca51a19095ceacb4637715b7acd40268cef0bb
blueseanz <Walter.FengNZ@gmail.com>
2018-06-06 19:38:47 +1200

    Commit Back

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.DAL
     public class SchoolContext : DbContext
     {
 
-        public SchoolContext() : base("SchoolContext")qqqq
+        public SchoolContext() : base("SchoolContext")
         {
         }
 
